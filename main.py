01da77fa206b72852f5068d29e5e8ab20e9a0388
MuhammadAmas <amaswaseem@gmail.com>
2023-09-16 19:24:25 +0500

    modular approach added

---
+++ main.py
@@ -0,0 +1,26 @@
+from RR import run_rr_scheduling
+from SRTF import run_srtf_scheduling
+from HRRN import run_hrrn_scheduling
+
+while True:
+    user_choice = input(
+        """Which scheduling algorithm do you want to use? 
+        (RR) -> Round Robin 
+        (SJF) -> Shortest Job First 
+        (HRRN) -> Highest Response Ratio Next 
+        (SRT) -> Shortest Remaining Time \n
+        \rType the algorithm name short form: """).lower()
+    print('\n\n')
+    if user_choice == "rr":
+        run_rr_scheduling()
+    elif user_choice == "hrrn":
+        run_hrrn_scheduling()
+        break
+    elif user_choice == "srt":
+        run_srtf_scheduling()
+        break
+    elif user_choice == "sjf":
+        print("SJF")
+        break
+    else:
+        print("Invalid choice. Please try again.")
