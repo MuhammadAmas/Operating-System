abbb2fb456b0d28e07bda81389ba685276f1586c
ilhaamsoomro <ilhaamsoomro@gmail.com>
2023-09-19 13:38:18 +0500

    updated

--- Scheduling Algorithm/HRRN.py
+++ Scheduling Algorithm/HRRN.py
@@ -1,8 +1,6 @@
 from tabulate import tabulate
 
 # Function to calculate the average waiting, turnaround, and completion time
-
-
 def calculate_average_times(waiting_time, turnaround_time, completion_time):
     n = len(waiting_time)
     avg_waiting_time = sum(waiting_time) / n
@@ -10,15 +8,45 @@ def calculate_average_times(waiting_time, turnaround_time, completion_time):
     avg_completion_time = sum(completion_time) / n
     return avg_waiting_time, avg_turnaround_time, avg_completion_time
 
+# Function to create a Gantt chart
+def create_gantt_chart(process, arrival_time, burst_time):
+    gantt_chart = []
+    start_time = []
+    end_time = []
+    start_time.append(arrival_time[0])
+    end_time.append(burst_time[0] + start_time[0])
+    for i in range (1 ,len(arrival_time)):
+        start_time.append((end_time[i-1]))
+        end_time.append(((burst_time[i])+(start_time[i])))
+    for p, start, end in zip(process, start_time, end_time):
+        print('p', p)
+        gantt_chart.append([p, start, end])
+    return gantt_chart
+
+# Function to display the Gantt chart
+def display_gantt_chart(gantt_chart):
+    for row in gantt_chart:
+        print(f"  {row[0]}" + " " * len(str(row[2] - row[1])), end="")
+    print()
+    for row in gantt_chart:
+        print("|" + "-" * (row[2] - row[1] + 1), end="")
+    print("|")
+    for row in gantt_chart:
+        print(f"{row[1]}", end="")
+        print(" " * (row[2] - row[1] - len(str(row[1]))), end="  ")
+    print(row[2], end="")
+    print()
+
+
 # Highest Response Ratio Next (HRRN) Scheduling
-
-
 def hrrn_scheduling(num_processes, arrival_time, burst_time):
     completed = [0] * num_processes
     waiting_time = [0] * num_processes
     turnaround_time = [0] * num_processes
     completion_time = [0] * num_processes
-    process = [f"p{i+1}" for i in range(num_processes)]
+    process = [f"P{i+1}" for i in range(num_processes)]
+    start_time = []
+    end_time = []
 
     sum_bt = sum(burst_time)
     avgwt = 0
@@ -26,6 +54,9 @@ def hrrn_scheduling(num_processes, arrival_time, burst_time):
     t = arrival_time[0]
 
     data = []
+    g_process=[]
+    g_burst=[]
+    g_arrival=[]
 
     while t < sum_bt:
         hrr = -9999
@@ -46,9 +77,14 @@ def hrrn_scheduling(num_processes, arrival_time, burst_time):
         turnaround_time[loc] = t - arrival_time[loc]
         completed[loc] = 1
         avgwt += waiting_time[loc]
+        start_time.append(t - burst_time[loc])
+        end_time.append(t)
 
         data.append([process[loc], arrival_time[loc], burst_time[loc],
                     waiting_time[loc], turnaround_time[loc], completion_time[loc]])
+        g_burst.append(burst_time[loc])
+        g_process.append(process[loc])
+        g_arrival.append(arrival_time[loc])
 
     avg_waiting_time, avg_turnaround_time, avg_completion_time = calculate_average_times(
         waiting_time, turnaround_time, completion_time)
@@ -58,11 +94,12 @@ def hrrn_scheduling(num_processes, arrival_time, burst_time):
     headers = ["Name", "Arrival Time", "Burst Time",
                "Waiting Time", "Turnaround Time", "Completion Time"]
 
+    display_gantt_chart(create_gantt_chart(g_process, g_arrival, g_burst))
     print(tabulate(data, headers, tablefmt="grid"))
+    
+
 
 # Wrap the code in a function
-
-
 def run_hrrn_scheduling():
     print("\n*~~~~~*~~~~~*~~~~~*~~~~~*~~~~~*~~~~~*~~~~~*~~~~~*~~~~~*~~~~~*~~~~~*")
     print("                    HRRN Scheduling Algorithm                        ")
@@ -79,17 +116,18 @@ def run_hrrn_scheduling():
     hrrn_scheduling(num_processes, arrival_time, burst_time)
 
 # Function to input process details
-
-
 def input_process_details():
     num_processes = int(input("Enter the number of processes: "))
     arrival_time = []
     burst_time = []
 
     for i in range(num_processes):
-        arrival = int(input(f"Enter arrival time for process p{i + 1}: "))
-        burst = int(input(f"Enter burst time for process p{i + 1}: "))
+        arrival = int(input(f"Enter arrival time for process P{i + 1}: "))
+        burst = int(input(f"Enter burst time for process P{i + 1}: "))
         arrival_time.append(arrival)
         burst_time.append(burst)
 
     return num_processes, arrival_time, burst_time
+
+# Run the HRRN scheduling algorithm
+run_hrrn_scheduling()
